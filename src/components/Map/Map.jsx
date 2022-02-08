d189ea4628c8b437a6208f2dd8fc58f538d4ec85
mamonten0k <asdnbvb123@gmail.com>
2022-02-08 22:15:42 +0300

    First Commit

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -18,7 +18,7 @@ const Map = ({ setCoordinates, setBounds, coordinates}) => {
 				margin={[50, 50, 50, 50]}
 				options={''}
 				onChange={(e) => {
-					setCoordinates({ lat: e.center.lat, lng: e.center.lng });
+					StCoordinates({ lat: e.center.lat, lng: e.center.lng });
 					setBounds({ ne: e.marginBounds.ne, sw: e.marginBounds.sw });
 				  }}
 				onChildClick={''}
